6dafdb47b5d0c609601a14fbc8e24f2867ed6d4a
lokesh0903sh <lokesh0903sh.com>
2026-06-07 23:09:35 +0530

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 export function Header() {
   return (
     <header className="animate-fade-in-up text-center">
-      <div className="mx-auto mb-4 flex justify-center">
-        <img src="/RMT_Medical_Store_Transparent.png" alt="RMT Medical Store Logo" className="h-32 object-contain" />
+      <div className="mx-auto flex justify-center">
+        <img src="/RMT_Medical_Store_Transparent.png" alt="RMT Medical Store Logo" className="h-56 sm:h-64 object-contain" />
       </div>
     </header>
   );
